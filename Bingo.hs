b995385fec50f1e035fb0754a722b5761b25e41e
mitsuji <mitsuji@na-s.jp>
2015-07-18 05:06:05 +0900

    refactoring

--- Bingo.hs
+++ Bingo.hs
@@ -1,16 +1,25 @@
 import qualified System.Random as R
-import Data.List(foldl')
-import Data.Monoid(Monoid,mempty,mappend,(<>),mconcat)
+import Data.Monoid(Monoid,mempty,mappend,mconcat)
+import qualified Lottery as Lot
 
-type Candidate = [Int]
-type Selection = [Int]
+
+
+
 type Card = ([Int],[Int])
-type CardResult = [Bool]
+
 
 data CardStatus = Bingo
                 | Lizhi Int
                 | Blank
                 deriving(Show)
+instance Monoid CardStatus where
+  mempty = Blank
+  mappend Bingo _ = Bingo
+  mappend _ Bingo = Bingo
+  mappend (Lizhi n1) (Lizhi n2) = Lizhi (n1 + n2)
+  mappend l@(Lizhi _) _ = l
+  mappend _ l@(Lizhi _) = l
+  mappend _  _ = Blank
 
 
 
@@ -59,11 +68,12 @@ main = do
 
 doBingo turn card1 card2 card3 s = do
   g <- R.newStdGen
-  let s'@(_,sel) = pickRandom g s
+  let (x,s'@(_,sel)) = Lot.draw g s
   let cr1 = processCard card1 sel
   let cr2 = processCard card2 sel
   let cr3 = processCard card3 sel
   
+  putStrLn $ (show turn) ++ ": x: " ++ (show x)
   putStrLn $ (show turn) ++ ": sel: " ++ (show sel)
   putStrLn $ (show turn) ++ ": card1: " ++ (show $ sbl cr1)
   putStrLn $ (show turn) ++ ": card2: " ++ (show $ sbl cr2)
@@ -74,88 +84,39 @@ doBingo turn card1 card2 card3 s = do
 
   return s'
   
+  where
+    sbl = map (\b -> case b of
+                  True -> 1
+                  False -> 0)
 
-sbl = map (\b -> case b of
-              True -> "T"
-              False -> "F")
 
 
+      
 newCandidate :: R.RandomGen g => g -> [Int]
-newCandidate g = selectRandom g [0..99] 30
+newCandidate g = Lot.pick g [0..99] 30
+
 
 newCard :: R.RandomGen g => g -> [Int] -> Card
-newCard g cs = splitAt 12 $ selectRandom g cs 24
+newCard g cs = splitAt 12 $ Lot.pick g cs 24
 
 
-pickRandom :: R.RandomGen g => g -> ([Int],[Int]) -> ([Int],[Int])
-pickRandom g (rem,sel) = ((reduce rem n),(rem !! n):sel)
-  where
-    (n,_) = R.randomR (0,((length rem)-1)) g
+processCard :: Card -> [Int] -> [Bool]
+processCard (sect1, sect2) sel =
+  (map (\i -> elem i sel ) sect1) ++ [True] ++ (map (\i -> elem i sel ) sect2)
 
 
---
--- xs から c個 ランダムに取り出す
---
-selectRandom :: R.RandomGen g => g -> [Int] -> Int -> [Int]
-selectRandom g xs c = gradualSelect xs (decrescIntRandom g ((length xs)-1) c) []
-
-
--- xs から {rsの要素}番目の値を順次取り出す
--- xs  候補リスト
--- rs  Int乱数リスト
--- acc アキュムレータ
-gradualSelect :: [a] -> [Int] -> [a] -> [a]
-gradualSelect _ [] acc = acc
-gradualSelect xs (r:rs) acc = gradualSelect (reduce xs r) rs ((xs !! r):acc)
-
-
--- [0 -> m, 0 -> (m-1), 0 -> (m-2), ...] と範囲が順次減少するInt乱数リストを c個生成
--- g 乱数発生器
--- m 上限値の初期値
--- c 生成するリストの長さ
-decrescIntRandom :: R.RandomGen g => g -> Int -> Int -> [Int]
-decrescIntRandom g m c = map ( \(i,f)-> toInt (m-i) f ) $ zip [0..(c-1)] (R.randoms g :: [Float])
-  where
-    toInt :: Int -> Float -> Int -- Floatの乱数(f)を上限mのInt乱数に変換
-    toInt m f = truncate $ (fromIntegral (m+1)) * f 
-
-
--- xs から n 番めを除去
-reduce :: [a] -> Int -> [a]
-reduce xs n = (take n xs) ++ (drop (n+1) xs)
-
-
-
-
-instance Monoid CardStatus where
-  mempty = Blank
-  mappend Bingo _ = Bingo
-  mappend _ Bingo = Bingo
-  mappend (Lizhi n1) (Lizhi n2) = Lizhi (n1 + n2)
-  mappend l@(Lizhi _) _ = l
-  mappend _ l@(Lizhi _) = l
-  mappend _  _ = Blank
-
-
-
-
-processCard :: Card -> Selection -> CardResult
-processCard (card1, card2) sel =
-  (map (\i -> elem i sel ) card1) ++ [True] ++ (map (\i -> elem i sel ) card2)
-
-
-evalCard :: CardResult -> CardStatus
+evalCard :: [Bool] -> CardStatus
 evalCard =
-  mconcat $ (map evalV [0..4]) ++ (map evalH [0..4]) ++ [evalL, evalR]
+  mconcat $ (map evalH [0..4]) ++ (map evalV [0..4]) ++ [evalLT2RB, evalRT2LB]
 
   
-evalV n cr = eval $ v n cr
-evalH n cr = eval $ h n cr
-evalL = eval . lTrB
-evalR = eval . rTlB
+evalH n cr = eval $ filterH n cr
+evalV n cr = eval $ filterV n cr
+evalLT2RB = eval . filterLT2RB
+evalRT2LB = eval . filterRT2LB
 
 
-eval :: CardResult -> CardStatus
+eval :: [Bool] -> CardStatus
 eval cr = 
   case length $ filter id cr of
     5 -> Bingo
@@ -163,26 +124,37 @@ eval cr =
     otherwise -> Blank
 
 
-v :: Int -> [a] -> [a]
-v y l = take 5 $ drop (y*5) l
+filterH :: Int -> [a] -> [a]
+filterH n xs = take l $ drop (n*l) xs
+  where
+    l = truncate $ sqrt $ fromIntegral $ length xs
 
-h :: Int -> [a] -> [a]
-h x l = foldr (\i acc -> (l !! ((i*5)+x)) : acc) [] [0..4]
+filterV :: Int -> [a] -> [a]
+filterV n xs = foldr (\i acc -> (xs !! ((i*l)+n)) : acc) [] [0..(l-1)]
+  where
+    l = truncate $ sqrt $ fromIntegral $ length xs
 
-lTrB :: [a] -> [a]
-lTrB l = foldr (\i acc -> (l !! (i*6)) : acc) [] [0..4]
+filterLT2RB :: [a] -> [a]
+filterLT2RB xs = foldr (\i acc -> (xs !! (i*(l+1))) : acc) [] [0..(l-1)]
+  where
+    l = truncate $ sqrt $ fromIntegral $ length xs
 
-rTlB :: [a] -> [a]
-rTlB l = foldr (\i acc -> (l !! ((i+1)*4)) : acc) [] [0..4]
+filterRT2LB :: [a] -> [a]
+filterRT2LB xs = foldr (\i acc -> (xs !! ((i+1)*(l-1))) : acc) [] [0..(l-1)]
+  where
+    l = truncate $ sqrt $ fromIntegral $ length xs
 
 
 
 
 test = do
-  let cr = processCard ([1,2,3,4,5,6,7,8,9,10,11,12],[14,15,16,17,18,19,20,21,22,23,24,25]) [1,2,4,5,7,8,9,10,6]
-  print cr
+  let cr = processCard ([1,2,3,4,5,6,7,8,9,10,11,12],[14,15,16,17,18,19,20,21,22,23,24,25]) (10:9:8:7:5:4:2:1:[])
+  print $ sbl cr
   print $ evalCard cr
-
+  where
+    sbl = map (\b -> case b of
+                  True -> 1
+                  False -> 0)
 
 testEc = do
   print $ evalCard $ map (==1) [1,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
@@ -192,44 +164,59 @@ testEc = do
   print $ evalCard $ map (==1) [1,1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,1,1,1]
   print $ evalCard $ map (==1) [1,1,0,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,1,1,1]
 
-testEv = do
-  print $ evalV 0 $ map (==1) [1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-  print $ evalV 1 $ map (==1) [0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-  print $ evalV 2 $ map (==1) [0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]
-  print $ evalV 3 $ map (==1) [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0]
-  print $ evalV 4 $ map (==1) [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0]
-
 testEh = do
-  print $ evalH 0 $ map (==1) [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
-  print $ evalH 1 $ map (==1) [0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0]
-  print $ evalH 2 $ map (==1) [0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0]
-  print $ evalH 3 $ map (==1) [0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0]
-  print $ evalH 4 $ map (==1) [0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0]
+  print $ evalH 0 $ map (==1) [1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  print $ evalH 1 $ map (==1) [0,0,0,0,0,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+  print $ evalH 2 $ map (==1) [0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]
+  print $ evalH 3 $ map (==1) [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0]
+  print $ evalH 4 $ map (==1) [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0]
+
+testEv = do
+  print $ evalV 0 $ map (==1) [1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
+  print $ evalV 1 $ map (==1) [0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0]
+  print $ evalV 2 $ map (==1) [0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0]
+  print $ evalV 3 $ map (==1) [0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0]
+  print $ evalV 4 $ map (==1) [0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0]
 
 testEl = do
-  print $ evalL $ map (==1) [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0]
+  print $ evalLT2RB $ map (==1) [1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0]
 
 testEr = do
-  print $ evalR $ map (==1) [0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0]
+  print $ evalRT2LB $ map (==1) [0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0]
+
+
+testh = do
+  print $ filterH 0 [1,2,3,4,5,6,7,8,9]
+  print $ filterH 1 [1,2,3,4,5,6,7,8,9]
+  print $ filterH 2 [1,2,3,4,5,6,7,8,9]
+
+  print $ filterH 0 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterH 1 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterH 2 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterH 3 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterH 4 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
 
 testv = do
-  print $ v 0 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ v 1 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ v 2 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ v 3 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ v 4 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 0 [1,2,3,4,5,6,7,8,9]
+  print $ filterV 1 [1,2,3,4,5,6,7,8,9]
+  print $ filterV 2 [1,2,3,4,5,6,7,8,9]
 
-testh = do
-  print $ h 0 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ h 1 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ h 2 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ h 3 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-  print $ h 4 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 0 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 1 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 2 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 3 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterV 4 [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
 
 testl = do
-  print $ lTrB [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterLT2RB [1,2,3,4,5,6,7,8,9]
+  
+  print $ filterLT2RB [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+
 
 testr = do
-  print $ rTlB [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+  print $ filterRT2LB [1,2,3,4,5,6,7,8,9]
+
+  print $ filterRT2LB [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
---
+++ Lottery.hs
@@ -0,0 +1,52 @@
+module Lottery (
+  draw,
+  pick
+  ) where
+
+
+import qualified System.Random as R
+
+
+
+
+--
+-- rem から 1個ランダムに取り出し, sel に追加する
+-- 取り出したものと新しい状態を返す
+--
+draw :: R.RandomGen g => g -> ([a],[a]) -> (a,([a],[a]))
+draw g (rem,sel) = (x,((reduce rem n),x:sel))
+  where
+    x = rem !! n
+    (n,_) = R.randomR (0,((length rem)-1)) g
+
+
+--
+-- xs から c個 ランダムに取り出す
+--
+pick :: R.RandomGen g => g -> [a] -> Int -> [a]
+pick g xs c = gradualSelect xs (decrescIntRandom g ((length xs)-1) c) []
+
+
+-- xs から {rsの要素}番目の値を順次取り出す
+-- xs  候補リスト
+-- rs  Int乱数リスト
+-- acc アキュムレータ
+gradualSelect :: [a] -> [Int] -> [a] -> [a]
+gradualSelect _ [] acc = acc
+gradualSelect xs (r:rs) acc = gradualSelect (reduce xs r) rs ((xs !! r):acc)
+
+
+-- [0 -> m, 0 -> (m-1), 0 -> (m-2), ...] と範囲が順次減少するInt乱数リストを c個生成
+-- g 乱数発生器
+-- m 上限値の初期値
+-- c 生成するリストの長さ
+decrescIntRandom :: R.RandomGen g => g -> Int -> Int -> [Int]
+decrescIntRandom g m c = map ( \(i,f)-> toInt (m-i) f ) $ zip [0..(c-1)] (R.randoms g :: [Float])
+  where
+    toInt :: Int -> Float -> Int -- Floatの乱数(f)を上限mのInt乱数に変換
+    toInt m f = truncate $ (fromIntegral (m+1)) * f 
+
+
+-- xs から n 番めを除去
+reduce :: [a] -> Int -> [a]
+reduce xs n = (take n xs) ++ (drop (n+1) xs)
